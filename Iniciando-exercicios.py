6230d588f7b04d7df5158df369221814f835e01a
Mateus Ramos Peiter <mateus29102809@gmail.com>
2026-01-12 01:40:00 -0300

    Add initial Python exercises and print statements

---
+++ Iniciando-exercicios.py
@@ -0,0 +1,13 @@
+# Aula 1 a 4: Primeiros passos
+print('Olá, Mundo!')
+
+# Aula 5 e 6: Interação e Tipos
+nome = input('Qual é o seu nome? ')
+print('É um prazer te conhecer, {}!'.format(nome))
+
+# Aula 7: Operações Simples
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+
+print('A soma entre {} e {} vale {}'.format(n1, n2, s))
