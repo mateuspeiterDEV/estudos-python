0d2cd78b9cec1e9da9c6e8bb63da6b0fc7ebdc61
Mateus Ramos Peiter <mateus29102809@gmail.com>
2026-01-13 20:48:12 -0300

    Add files via upload

---
+++ python.1.py
@@ -0,0 +1,11 @@
+nome = input('Olá, qual o seu nome? ')
+print('Olá {}, esse é o meu primeiro código! '.format(nome))
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+print('Os valores são {}, a soma é {}, a multiplicação é {}! '.format(n1+n2, s, m))
+print('a divisão é {} e a divisão inteira é {}, espero que tenha gostado! '.format(d, di)) 
+input('pressione uma tecla para sair...')
